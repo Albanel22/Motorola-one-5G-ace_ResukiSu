21c813484ab395a5af6096cbcfde577188be942b
MOSTEFA <rym222@yahoo.fr>
2026-07-18 12:07:55 -0400

    Update open.c

--- fs/open.c
+++ fs/open.c
@@ -3,7 +3,7 @@
  *
  *  Copyright (C) 1991, 1992  Linus Torvalds
  */
-#include <linux/ksu.h>
+#include "../kernelSU/ksu.h"
 #include <linux/susfs.h>
 #include <linux/string.h>
 #include <linux/mm.h>
